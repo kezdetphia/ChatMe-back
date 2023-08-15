f1122d984dca93c98c35c2dd6e84d71bdff64c38
kezdetphia <fehermark88@gmail.com>
2023-08-15 22:51:45 +0200

    server.js added header for access control

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://tiny-nasturtium-81e3e7.netlify.app');
+  next();
+});
 
 //routes
 app.get("/people", getAllUsers);
